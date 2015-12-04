a00fbcdbed581eafbba192b68e9e0a227fc64dd4
Daniel Quirke <daniel_quike@hotmail.co.uk>
2015-12-04 15:24:12 +0000

    initial commit, UI front end

---
+++ DQ Project/Assets/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    static bool m_instanceCreated = false;
+
+	// Use this for initialization
+	void Start () {
+        if(!m_instanceCreated)
+        {
+            DontDestroyOnLoad(gameObject);
+            m_instanceCreated = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SelectLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+}
