0d356a3fcfbdb233b1285700c4bb58eabb2d24f9
maxaction <maxaction@users.noreply.github.com>
2015-12-05 15:33:59 +0000

    added my character controller and test scene

---
+++ Game/Assets/Scripts/Player/Camera_Movement.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class Camera_Movement : MonoBehaviour {
+
+
+    public float LookSpeed_mulitplier = 1.0f;
+    public float max_Vertical_Angle = 90.0f;
+    public float min_Vertical_Angle = .0f;
+    public float Close_Zoom_Dist = 5.0f;
+    public float Far_Zoom_Dist = 20.0f;
+
+    public GameObject Camera_object = null;
+
+
+    private Vector3 m_rotationYAxis;
+    private Vector3 m_rotationXAxis;
+    private Vector3 m_rotationZAxis;
+    private float m_Angle;
+    private bool m_zoomedIn = false;
+
+    private GamePadState state;
+    private GamePadState prevState;
+    private PlayerIndex playerIndex;
+    private bool playerIndexSet = false;
+
+	// Use this for initialization
+	void Start () {
+
+        //Y- axis set up
+        m_rotationYAxis.x = 0;
+        m_rotationYAxis.y = 1;
+        m_rotationYAxis.z = 0;
+
+        //x- axis set up
+        m_rotationXAxis.x = 0;
+        m_rotationXAxis.y = 0;
+        m_rotationXAxis.z = 1;
+
+        //z-axis set up
+        m_rotationZAxis.x = 0;
+        m_rotationZAxis.y = 0;
+        m_rotationZAxis.z = 1;
+        m_Angle = transform.eulerAngles.z;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+
+
+
+        float RightStick_yAxis = state.ThumbSticks.Right.Y;
+
+
+        //transform.Rotate(m_rotationXAxis, RightStick_yAxis * LookSpeed_mulitplier);
+        m_Angle += RightStick_yAxis * LookSpeed_mulitplier;
+
+        if (m_Angle < min_Vertical_Angle)
+        {
+            m_Angle = min_Vertical_Angle;
+        }
+        else if (m_Angle > max_Vertical_Angle)
+        {
+            m_Angle = max_Vertical_Angle;
+        }
+
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, m_Angle);
+
+        if (Camera_object != null)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Joystick1Button3))
+            {
+
+                if (m_zoomedIn)
+                {
+                    Camera_object.transform.localPosition = new Vector3(Far_Zoom_Dist, 0, 0);
+                }
+                else
+                {
+                    Camera_object.transform.localPosition = new Vector3(Close_Zoom_Dist, 0, 0);
+                }
+
+                m_zoomedIn = !m_zoomedIn;
+            }
+        }
+       
+    }
+}
---
+++ Game/Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class Player_Movement : MonoBehaviour {
+
+    
+
+   // public float TurnSpeed_Mulitplier = 1.0f;
+    public float MoveSpeed_mulitplier = 1.0f;
+
+
+    private GameObject Front;
+
+    private Vector3 m_rotationAxis;
+
+    private GamePadState state;
+    private GamePadState prevState;
+    private PlayerIndex playerIndex;
+    private bool playerIndexSet = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Front = GameObject.Find("Front");
+        m_rotationAxis.y = 1;
+        m_rotationAxis.x = 0;
+        m_rotationAxis.z = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        float LeftStick_xAxis = state.ThumbSticks.Left.X;
+        float LeftStick_yAxis = state.ThumbSticks.Left.Y;  
+    
+        if(Input.GetKey(KeyCode.W))
+        {
+            LeftStick_yAxis = 1;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            LeftStick_yAxis = -1;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            LeftStick_xAxis = 1;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            LeftStick_xAxis = -1;
+        }
+
+        if ((prevState.Buttons.RightShoulder == ButtonState.Released && state.Buttons.RightShoulder == ButtonState.Pressed) || Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.Rotate(m_rotationAxis, 90);
+        }
+        else if ((prevState.Buttons.LeftShoulder == ButtonState.Released && state.Buttons.LeftShoulder == ButtonState.Pressed) || Input.GetKeyDown(KeyCode.E))
+        {
+            transform.Rotate(m_rotationAxis, -90);
+        }
+        //transform.Rotate(m_rotationAxis, RightStick_xAxis * TurnSpeed_Mulitplier);
+
+        Vector3 Direction = Front.transform.position - transform.position;
+
+        Direction.Normalize();
+
+        Vector3 DirectionRight = new Vector3(Direction.z, Direction.y, -Direction.x);
+
+        DirectionRight *= LeftStick_xAxis;
+        Direction *= LeftStick_yAxis;
+        Vector3 TotalDirection = Direction + DirectionRight;
+
+        TotalDirection.Normalize();
+
+        TotalDirection *= Time.deltaTime;
+
+        transform.position += (TotalDirection) * MoveSpeed_mulitplier;
+	}
+}
