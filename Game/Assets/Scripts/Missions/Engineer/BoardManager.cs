6ebc5455beea1e643060adc03822eb119518543f
djquirke <daniel_quirke@hotmail.co.uk>
2016-01-25 23:49:38 +0000

    pipe minigame mid complete
    next step: randomly generate start/end point and then devise algorithm
    to go from start point to end point, highlighting the path on the way

--- Game/Assets/Scripts/Missions/Engineer/BoardManager.cs
+++ Game/Assets/Scripts/Missions/Engineer/BoardManager.cs
@@ -5,37 +5,86 @@ using System.Collections.Generic;
 public class BoardManager : MonoBehaviour {
 
 	private List<List<GameObject>> tiles; //<width<height>>
-	private int board_width = 9, board_height = 9;
+	public GameObject start_tile, end_tile;
+	private int board_width = 11, board_height = 11;
 	public GameObject TileNE, TileNES, TileNESW, TileNS;
 	//just one path to connect?
 
 
 	// Use this for initialization
 	void Start () {
+		tiles = new List<List<GameObject>>();
 		Initialise();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	public void Initialise()
 	{
+
+		RectTransform cvs_rtransform = gameObject.GetComponent<RectTransform>();
+		start_tile = (GameObject)Instantiate (start_tile);
+		start_tile.GetComponent<TileStart>().Initialise(2);
+		RectTransform tile_rtransform = start_tile.GetComponent<RectTransform>();
+
+		start_tile.transform.SetParent(gameObject.transform, false);
+		start_tile.transform.position = new Vector3(cvs_rtransform.rect.width / 2, cvs_rtransform.rect.height / 2);
+		start_tile.GetComponent<TileStart>().setPos(tile_rtransform);
+		 
 		for(int i = 0; i < board_width; i++)
 		{
+			List<GameObject> tempList = new List<GameObject>();
 			for(int j = 0; j < board_height; j++)
 			{
-				GameObject new_tile = (GameObject)Instantiate(TileNES, gameObject.transform.position, gameObject.transform.rotation);
-				new_tile.transform.SetParent(gameObject.transform, false);
-				RectTransform cvs_rtransform = gameObject.GetComponent<RectTransform>();
-				RectTransform tile_rtransform = new_tile.GetComponent<RectTransform>();
-				new_tile.transform.position = 
-					new Vector3((cvs_rtransform.rect.width / 2) - (4 - i) * tile_rtransform.rect.width / 2,
-					            (cvs_rtransform.rect.height / 2) + (4 - j) * tile_rtransform.rect.height / 2);
-				Debug.Log("tile width: " + tile_rtransform.rect.width + ", tile height: " + tile_rtransform.rect.height);
-			}
-		}
+				GameObject new_tile;
 
+				if( i == 0 || i == 10 || j == 0 || j == 10)
+				{
+					new_tile = new GameObject();
+					new_tile.transform.SetParent(gameObject.transform, false);
+					tempList.Add(new_tile);
+					continue; 
+				}
+
+				switch (Random.Range(0, 3))
+				{
+				case 0:
+					new_tile = (GameObject)Instantiate(TileNE);
+					break;
+				case 1:
+					new_tile = (GameObject)Instantiate(TileNES);
+					break;
+				case 2:
+					new_tile = (GameObject)Instantiate(TileNESW);
+					break;
+				default:
+					new_tile = (GameObject)Instantiate(TileNS);
+					break;
+				}
+
+				new_tile.transform.SetParent(gameObject.transform, false); //make child of canvas
+				new_tile.transform.position = 
+					new Vector3((cvs_rtransform.rect.width / 2) - (5 - i) * tile_rtransform.rect.width / 2,
+					            (cvs_rtransform.rect.height / 2) + (5 - j) * tile_rtransform.rect.height / 2);
+				Debug.Log("tile width: " + tile_rtransform.rect.size.x + ", tile height: " + tile_rtransform.rect.size.y);
+
+				tempList.Add(new_tile);
+			}
+			tiles.Add(tempList);
+		}
+	}
+
+	public void CheckFlow()
+	{
+		Exits exits1 = tiles[1][1].GetComponent<Tile>().Exit();
+		Exits exits2 = tiles[2][1].GetComponent<Tile>().Exit();
+		if(exits1.east && exits2.west)
+		{
+			tiles[1][1].GetComponent<Tile>().setFlow(true);
+			tiles[2][1].GetComponent<Tile>().setFlow(true);
+		}
+		else
+		{
+			tiles[1][1].GetComponent<Tile>().setFlow(false);
+			tiles[2][1].GetComponent<Tile>().setFlow(false);
+		}
 	}
 }
---
+++ Game/Assets/Scripts/Missions/Engineer/TileStart.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileStart : Tile {
+
+	int startHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Initialise(int height)
+	{
+		startHeight = height;
+	}
+
+	public int Height()
+	{
+		return startHeight;
+	}
+
+	public void setPos(RectTransform tile_dims)
+	{
+		this.transform.position = 
+			new Vector3(this.transform.position.x - 5 * tile_dims.rect.width / 2,
+			            this.transform.position.y + (5 - startHeight) * tile_dims.rect.height / 2);
+
+	}
+
+
+}
