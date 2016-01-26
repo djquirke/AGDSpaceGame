1ffe91e6519e6e418174ea504731ece4cd73271e
djquirke <daniel_quirke@hotmail.co.uk>
2016-01-26 15:46:26 +0000

    Merge branch 'master' of https://github.com/djquirke/AGDSpaceGame
    # Conflicts:
    #	Game/ProjectSettings/TagManager.asset

---
+++ Game/Assets/Scripts/Missions/Illness/EventTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTrigger : MonoBehaviour {
+
+    public EventBase EventCode = null;
+
+
+	void OnTriggerEnter(Collider Other)
+    {
+        EventCode.Activate();
+    }
+}
