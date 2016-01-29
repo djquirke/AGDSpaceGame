2f9d7d2b1a1a0a9ac07cecf5736cc75b9a899dcc
Matthew Berry <howling.techie@gmail.com>
2016-01-29 16:39:46 +0000

    Rooms

---
+++ Game/Assets/Room/RoomSplitter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomSplitter : MonoBehaviour
+{
+    public GameObject[] roomsets;
+    public int[] weights;
+
+    List<int> weightsList = new List<int>();
+
+    // Use this for initialization
+    void Start()
+    {
+        Split();
+        Destroy(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Split()
+    {
+        for (int i = 0; i < weights.Length; i++)
+        {
+            AddWeight(weights[i], i);
+        }
+
+
+        int rand = Random.Range(0, weightsList.Count);
+
+        if (roomsets[weightsList[rand]] == null)
+        {
+            return;
+        }
+        else
+        {
+            GameObject rooms = (GameObject)Instantiate(roomsets[weightsList[rand]], transform.position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+        }
+
+
+        Destroy(gameObject);
+    }
+
+    void AddWeight(int weight, int value)
+    {
+        for (int i = 0; i < weight; i++)
+        {
+            weightsList.Add(value);
+        }
+    }
+}
