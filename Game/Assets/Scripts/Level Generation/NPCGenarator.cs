6a0f5223c3432e3a3b63ae51404e884093e734ac
maxaction <maxaction@users.noreply.github.com>
2016-02-12 19:48:50 +0000

    NPC Generator implemented
    Issue: the Rooms don't have collisions

---
+++ Game/Assets/Scripts/Level Generation/NPCGenarator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCGenarator : MonoBehaviour {
+
+    public GameObject[] NPCsets;
+    public int[] weights;
+    public int NumberOfNPCs = 1;
+
+    List<int> weightsList = new List<int>();
+
+    // Use this for initialization
+    void Start()
+    {
+        while(0!=NumberOfNPCs--)
+            Genarate();
+        
+    }
+
+    void Genarate()
+    {
+        for (int i = 0; i < weights.Length; i++)
+        {
+            AddWeight(weights[i], i);
+        }
+
+
+        int rand = Random.Range(0, weightsList.Count);
+
+        if (NPCsets[weightsList[rand]] == null)
+        {
+            return;
+        }
+        else
+        {
+            GameObject rooms = (GameObject)Instantiate(NPCsets[weightsList[rand]], transform.position + new Vector3(0f,3f,0), Quaternion.Euler(0.0f, 0.0f, 0.0f));
+        }
+    }
+
+    void AddWeight(int weight, int value)
+    {
+        for (int i = 0; i < weight; i++)
+        {
+            weightsList.Add(value);
+        }
+    }
+}
