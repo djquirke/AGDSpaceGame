528890fbc61501bdd9b940b4d462908694039b0c
Peter Southward <petersouthward@hotmail.com>
2016-03-29 20:58:29 +0100

    added the settings menu switching

---
+++ Game/Assets/Scripts/Menus/Options/MenuSwitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSwitch : MonoBehaviour {
+
+    private GameObject CurrentMenu = null;
+
+    public GameObject Parentobject = null;
+   
+
+    public void SetMenu(Object newMenu)
+    {
+
+        if(CurrentMenu != null)
+        {
+            DestroyImmediate(CurrentMenu);
+        }
+
+        CurrentMenu = (GameObject)Instantiate(newMenu);
+       
+        CurrentMenu.transform.SetParent(Parentobject.transform,false);
+    }
+
+
+}
