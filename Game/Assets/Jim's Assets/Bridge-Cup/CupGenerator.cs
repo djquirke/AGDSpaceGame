47ba4d0fc28403d9e2665eec95990d6e5ecbeb01
djquirke <daniel_quirke@hotmail.co.uk>
2016-04-23 12:28:39 +0100

    Assets part 1

---
+++ Game/Assets/Jim's Assets/Bridge-Cup/CupGenerator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CupGenerator : MonoBehaviour {
+    public Material[] mats;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material = mats[Random.Range(0,mats.Length)];
+	}
+	
+}
