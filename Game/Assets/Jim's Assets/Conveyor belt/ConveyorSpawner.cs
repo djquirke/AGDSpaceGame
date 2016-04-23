9f1384fe92054a618a93c8767eaab46a8d718320
djquirke <daniel_quirke@hotmail.co.uk>
2016-04-23 12:39:58 +0100

    Assets part 2

---
+++ Game/Assets/Jim's Assets/Conveyor belt/ConveyorSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorSpawner : MonoBehaviour {
+    public bool SpawnOrDestroy = false;
+    private bool isBox = false;
+    public GameObject targetZone;
+    public GameObject box;
+	// Use this for initialization
+	void Start () {
+        
+            StartCoroutine(Delayed());
+           
+            /*
+           Rigidbody rigid = box2.gameObject.AddComponent("Rigidbody") as Rigidbody;
+           BoxCollider boxCol = box2.gameObject.AddComponent("BoxCollider") as BoxCollider;
+           rigid.useGravity = false;
+           rigid.constraints = RigidbodyConstraints.FreezeRotation;
+           rigid.constraints = RigidbodyConstraints.FreezePosition;
+           sc.isBox = true;
+            * */
+
+        
+	}
+    IEnumerator Delayed(){
+       
+        if (!isBox & SpawnOrDestroy){
+            GameObject box2 = Instantiate(box, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            box2.transform.parent = transform;
+          
+
+            box2.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, (transform.position.z));
+            box2.transform.localPosition -= new Vector3(0.28f, 0f, 0f);
+            box2.transform.rotation = transform.rotation;
+            ConveyorSpawner sc = box2.gameObject.AddComponent("ConveyorSpawner") as ConveyorSpawner;
+            sc.isBox = true;
+            sc.targetZone = targetZone;
+            
+            //
+            yield return new WaitForSeconds(Random.Range(3, 15));
+            StartCoroutine(Delayed());
+        }
+        
+    }
+	// Update is called once per frame
+	void Update () {
+        if (isBox)
+        {
+            float dist = Vector3.Distance(targetZone.transform.position, transform.position);
+            if (dist < 0.6)
+            {
+                Destroy(gameObject);
+            }
+            transform.localPosition -= new Vector3(0.005f, 0f, 0f);
+        }
+	}
+}
---
+++ Game/Assets/Jim's Assets/SleepingQuarters-InnerWalls/WallCleanup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCleanup : MonoBehaviour {
+    public GameObject[] gList;
+	// Use this for initialization
+	void Start () {
+	    gList = GameObject.FindGameObjectsWithTag("Wall");
+        for (var i = 0; i < gList.Length; i += 1){
+            hasOther(gList, gList[i]);
+        }
+	}
+	public bool hasOther(GameObject[] List, GameObject g){
+        bool isFound = false;
+        for (var i = 0; i < List.Length - 1; i += 1)
+        {
+            if (List[i] == g)
+            {
+                Debug.Log("asd");
+            }
+        }
+        return isFound;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
