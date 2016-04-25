69f59d02e264e12f5401d911b2884d52d7c0df97
Peter Southward <petersouthward@hotmail.com>
2016-04-25 13:51:32 +0100

    added player animations

---
+++ Game/Assets/AnimationMaster/CharacterAnimController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimController : MonoBehaviour {
+    public float  max, current = 0;
+    private float weight = 0.0f;
+    private Animator anim;
+	void Start () {
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        float per = current / max;
+        if (per < 0.33) {
+            weight = per * 3;
+
+            anim.SetLayerWeight(1, weight);
+            anim.SetLayerWeight(2, 0.0f);
+        }
+        else if (per > 0.33)
+        {
+            per = Mathf.Min(0.66f, per);
+            weight = (per - 0.33f) * 3;
+            anim.SetLayerWeight(1, 1.0f);
+            anim.SetLayerWeight(2, weight);
+        }    
+	}
+}
