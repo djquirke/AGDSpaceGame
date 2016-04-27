f096c1f1536cea51995268e4bcf67d01878032d1
djquirke <daniel_quirke@hotmail.co.uk>
2016-04-27 05:42:37 +0100

    art pass 2
    decals, new models and minor additions that add some 'used' feeling to
    the ship.

---
+++ Game/Assets/Jim's Assets/Decal/DecalController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+ using System.Collections.Generic;
+using System.Collections.Generic;
+public class DecalController : MonoBehaviour {
+    public bool SpriteOverride = false;
+    public Sprite SpriteImage;
+    public Sprite[] RoomNumbers;
+    public float gen = 0.0f;
+    public List<float> genList = new List<float>();
+    public bool locked = false;
+	// Use this for initialization
+	void Start () {
+        if (SpriteOverride)
+        {
+            SpriteRenderer spt = GetComponent<SpriteRenderer>();
+            spt.sprite = SpriteImage;
+        }
+        else
+        {
+
+            gen = Random.Range(0.0f, 0.1f);
+            StartCoroutine(sortDecals());
+            //GameObject[] DecalNumbers = GameObject.FindGameObjectsWithTag("Decal");
+            //foreach (GameObject DecalObj in DecalNumbers) {
+            //    DecalController targetScript = DecalObj.GetComponent<DecalController>();
+            //    if
+            //}
+        }
+	}
+    IEnumerator sortDecals()
+    {
+        
+        yield return new WaitForSeconds(2);
+        GameObject[] DecalNumbers = GameObject.FindGameObjectsWithTag("Decal");
+        foreach (GameObject DecalObj in DecalNumbers)
+        {
+            DecalController targetScript = DecalObj.GetComponent<DecalController>() as DecalController;
+            if (!targetScript.SpriteOverride)
+            {
+                genList.Add(targetScript.gen);
+            }
+        }
+        genList.Sort();
+        int i = 1;
+        while (i < genList.Count && i < RoomNumbers.Length)
+        {
+            Debug.Log(genList[i]);
+            if (genList[i] == gen)
+            {
+                SpriteRenderer spt = GetComponent<SpriteRenderer>();
+                spt.sprite = RoomNumbers[i];
+                locked = true;
+            }
+            i += 1;
+        }
+        if (!locked)
+        {
+            Destroy(gameObject);
+        }
+        /*
+        yield return new WaitForSeconds(1);
+        GameObject[] DecalNumbers = GameObject.FindGameObjectsWithTag("Decal");
+        genlist = new float[DecalNumbers.Length];
+        int i = 0;
+        foreach (GameObject DecalObj in DecalNumbers)
+        {
+
+            DecalController targetScript = DecalObj.GetComponent<DecalController>() as DecalController;
+            if (!targetScript.SpriteOverride)
+            {
+                genlist[i] = targetScript.gen;
+            }
+            i+=1;
+        }
+        i = 0;
+        System.Array.Sort(genlist);
+        while (i < RoomNumbers.Length && i < genlist.Length)
+        {
+            if (genlist[i] == gen)
+            {
+                SpriteRenderer spt = GetComponent<SpriteRenderer>();
+                spt.sprite = RoomNumbers[i];
+                locked = true;
+            }
+            i += 1;
+            
+        }
+        if (!locked)
+        {
+            Destroy(gameObject);
+        }
+         * */
+        Debug.Log(transform.name + "  Decal Done");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
